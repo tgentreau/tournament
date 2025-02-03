89098b987a679115e9c38bd209120307ca7f3f4d
tgentreau <gentreau.thomas@gmail.com>
2025-02-03 14:48:56 +0100

    [feat]: add exceptions from issue 1 and 2

--- src/tournament/tournament.service.ts
+++ src/tournament/tournament.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTournamentDto } from './dto/create-tournament.dto';
 import { UpdateTournamentDto } from './dto/update-tournament.dto';
 import { TournamentRepository } from './tournament.repository';
@@ -8,6 +8,16 @@ import { Tournament } from './entities/tournament.entity';
 export class TournamentService {
   constructor(private readonly tournamentRepository: TournamentRepository) {}
   create(createTournamentDto: CreateTournamentDto): string {
+    if (createTournamentDto.name === '') {
+      throw new BadRequestException('Le nom est vide');
+    }
+    const tournaments: Tournament[] =
+      this.tournamentRepository.getAllTournaments();
+    for (const tournament of tournaments) {
+      if (tournament.name === createTournamentDto.name) {
+        throw new BadRequestException('Le nom est déjà utilisé');
+      }
+    }
     const tournament: Tournament = new Tournament(createTournamentDto);
     return this.tournamentRepository.saveTournament(tournament);
   }
@@ -17,7 +27,11 @@ export class TournamentService {
   }
 
   findOne(id: string): Tournament {
-    return this.tournamentRepository.getTournament(id);
+    const tournament: Tournament = this.tournamentRepository.getTournament(id);
+    if (tournament === undefined) {
+      throw new NotFoundException("Le tournoi n'existe pas");
+    }
+    return tournament;
   }
 
   update(id: string, updateTournamentDto: UpdateTournamentDto) {
